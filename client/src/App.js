d4d63e3cd72ae54012be190a30f6061206df7930
Elliot Glasenk <elliotglasenk@Elliots-MacBook-Pro.local>
2019-07-22 23:21:00 -0700

    Add Chart.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dropdown from './components/Dropdown';
-import Graph from './components/Graph';
+// import Graph from './components/Graph';
+import Chart from './components/Chart';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       results: [],
       dropdownValue: '',
-      result: {}
+      result: {},
+      chartData: {}
     };
   }
 
@@ -30,6 +32,40 @@ class App extends React.Component {
       dropdownValue: responseData.results[0].companyName,
       result: responseData.results[0]
     });
+
+  }
+
+  componentWillMount() {
+    this.getChartData()
+  }
+
+  getChartData() {
+    this.setState({
+      chartData: {
+        labels: ['Boston1', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets: [{
+          label: 'Population',
+          data: [
+            617594,
+            181045,
+            153060,
+            106519,
+            105162,
+            95072
+          ],
+          //backgroundColor:'green',
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(54, 162, 235, 0.6)',
+            'rgba(255, 206, 86, 0.6)',
+            'rgba(75, 192, 192, 0.6)',
+            'rgba(153, 102, 255, 0.6)',
+            'rgba(255, 159, 64, 0.6)',
+            'rgba(255, 99, 132, 0.6)'
+          ]
+        }]
+      }
+    })
   }
 
   //settings
@@ -57,7 +93,9 @@ class App extends React.Component {
           handleDropdownChange={this.handleDropdownChange}
           dropdownValue={this.state.dropdownValue}
         />
-        <Graph result={this.state.result} />
+        {/* <Graph result={this.state.result} /> */}
+        <Chart legendPosition="bottom" chartData={this.state.chartData} location="Massachusetts" />
+        {/* <Chart legendPosition="top" chartData={this.state.chartData} location="New York" /> */}
       </div>
     );
   }
